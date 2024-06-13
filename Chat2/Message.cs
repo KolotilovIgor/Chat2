aab0fdde1de0f47f86e89a9d1e9b655b3b1ca796
IgorK <gosha424@mail.ru>
2024-06-13 17:41:18 +0300

    Добавьте файлы проекта.

---
+++ Chat2/Message.cs
@@ -0,0 +1,27 @@
+﻿using Chat2;
+using System;
+using System.Text.Json;
+
+namespace Chat2
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string NicknameFrom { get; set; }
+        public string NicknameTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+        public static Message DeserializeFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{this.DateTime} получено сообщение {this.Text} от {this.NicknameFrom}";
+        }
+    }
+}
